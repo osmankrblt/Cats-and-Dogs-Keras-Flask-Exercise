3ce461ee9819b9e7dc2944914ec40e15832736d8
Osman Karabulut <h.osmankarabulut.fb67@hotmail.com>
2022-10-16 11:10:57 +0300

    .

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN apt-get install -y python-pip python-dev build-essential
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-CMD ["python", "wsgi.py"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -85,3 +85,7 @@ def home():
     return render_template("mainPage.html") 
     
         
+if __name__ == '__main__':
+    import os 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
